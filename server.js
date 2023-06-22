6a4d315e5f8f1afd965838360ead187a9d9a2850
Sharvil Nichat <sharvil.22111277@viit.ac.in>
2023-06-23 02:41:16 +0530

    Schema integrated and create user function written

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+// import { createDocument } from "./models";
+
 const express = require("express");
 const  mongoose = require("mongoose");
 const app = express();
 
-
+const createDocument = require('./models.js');
 // Database 
 const database = module.exports = () => {
   const connectionParams  = {
     useNewUrlParser :true,
     useUnifiedTopology:true,
+    
   }
 
   try{
     mongoose.connect('mongodb+srv://sharvil:sharvil123@jamshack-hackathon.lnxdgxj.mongodb.net/hackathon?retryWrites=true&w=majority');
     console.log('Database connected sucessfully');
+    createDocument();
   }
   catch(error){
     console.log(error);
