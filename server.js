7045ca4c09e2c1803f6b4f43eb9696f08830c177
Rutuja <smartrb4@gmail.com>
2023-06-23 18:58:56 +0530

    insert

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const database = module.exports = () => {
   }
 
   try{
-    mongoose.connect('mongodb+srv://sharvil:sharvil123@jamshack-hackathon.lnxdgxj.mongodb.net/hackathon?retryWrites=true&w=majority');
+    mongoose.connect('mongodb+srv://rutuja:rutuja123@jamshack-hackathon.lnxdgxj.mongodb.net/hackathon?retryWrites=true&w=majority');
     console.log('Database connected sucessfully');
+    // createDocument();
+    const createDocument = require('./models.js');
     createDocument();
   }
   catch(error){
@@ -26,8 +28,8 @@ const database = module.exports = () => {
 
 database();
 
-app.listen(5000, () => {
-  console.log("Server is running at port 5000");
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
 });
 
 
